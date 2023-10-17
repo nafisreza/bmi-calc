978691d0202896a7a4e33509705095e4468930d8
nafisreza <bigganinafis@gmail.com>
2023-10-18 04:47:21 +0600

    ⚡️ Feedback generator added

--- script.js
+++ script.js
@@ -4,10 +4,19 @@ btn.addEventListener('click', function(e){
     e.preventDefault()
     const height = document.querySelector("#height").value 
     const weight = document.querySelector("#weight").value
-    const result = document.querySelector("#results")
+    const result = document.querySelector("#result")
+    const feedback = document.querySelector("#feedback")
 
     const bmi = parseFloat(weight/((height*height)/10000)).toFixed(1)
-    console.log(bmi)
+
     result.style.fontSize = "20px"
     result.innerText = `Your BMI is ${bmi}` 
+
+    if(bmi < 18.5){
+        feedback.innerText = `You are underweight. Eat More.`
+    } else if(bmi >= 18.5 && bmi <= 24.9){
+        feedback.innerText = `You are normal. Keep it up.`
+    }else if(bmi > 24.9){
+        feedback.innerText = `You are overweight. Eat less.`
+    }
 })
